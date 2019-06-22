23da227a45bc585cea9d7e7fe9ba705ad9fccb8e
Félix Luján <lujan.felix80@gmamil.com>
2019-06-22 14:25:53 +0200

    created librery

---
+++ src/LdapClient.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace flujan\LdapClient;
+
+class LdapClient
+{
+    private $connection;
+    private $result;
+
+    /**
+     * @param string $server
+     * @param int $port
+     * @param string $user
+     * @param string $password
+     * @param array $options
+     * @return bool
+     */
+    public function connection(string $server, int $port, string $user, string $password, array $options = []): bool
+    {
+        $this->connection = ldap_connect($server, $port);
+        foreach ($options as $key => $option) {
+            ldap_set_option($this->connection, $key, $option);
+        }
+        return @ldap_bind($this->connection, $user, $password);
+    }
+
+    /**
+     * @param string $baseDn
+     * @param string $searchFilter
+     * @return bool
+     */
+    public function search(string $baseDn, string $searchFilter): bool
+    {
+        $this->result = @ldap_search($this->connection, $baseDn, $searchFilter);
+        return ( $this->result == false) ? false : true;
+    }
+
+    /**
+     * @return array
+     */
+    public function getEntries(): array
+    {
+        return $this->result ? @ldap_get_entries($this->connection, $this->result) : [];
+    }
+
+    /**
+     * @return bool
+     */
+    public function disconnection(): bool
+    {
+        return ($this->connection) ? @ldap_unbind($this->connection) : false;
+    }
+}
---
+++ tests/LdapClientTest.php
@@ -0,0 +1,91 @@
+<?php
+declare(strict_types=1);
+
+namespace flujan\LdapClient;
+
+use PHPUnit\Framework\TestCase;
+
+
+class LdapClientTest extends TestCase
+{
+    private $options = [ LDAP_OPT_PROTOCOL_VERSION => 3, LDAP_OPT_REFERRALS => 0];
+    private $user = 'cn=read-only-admin,dc=example,dc=com';
+    private $password = 'password';
+    private $port = 389;
+    private $server = 'ldap.forumsys.com';
+    private $baseDn = 'dc=example,dc=com';
+    private $filter = 'uid=riemann';
+
+
+    public function testConnectionSuccess()
+    {
+        $ldap  = new LdapClient();
+        $connection =$ldap->connection($this->server, $this->port, $this->user, $this->password, $this->options);
+        $this->assertTrue($connection);
+
+    }
+
+    public function testConnectionFail()
+    {
+        $ldap  = new LdapClient();
+        $connection =$ldap->connection($this->server, $this->port, $this->user, $this->password.'1', $this->options);
+        $this->assertFalse($connection);
+
+    }
+
+
+    public function testSearchSuccess()
+    {
+        $ldap  = new LdapClient();
+        $ldap->connection($this->server, $this->port, $this->user, $this->password, $this->options);
+        $result = $ldap->search($this->baseDn, $this->filter);
+        $this->assertTrue($result);
+    }
+
+    public function testSearchFail()
+    {
+        $ldap  = new LdapClient();
+        $ldap->connection($this->server, $this->port, $this->user, $this->password, $this->options);
+        $result = $ldap->search($this->baseDn.'1', $this->filter);
+        $this->assertFalse($result);
+    }
+
+    public function testGetEntitiesSuccess()
+    {
+        $ldap  = new LdapClient();
+        $ldap->connection($this->server, $this->port, $this->user, $this->password, $this->options);
+        $ldap->search($this->baseDn, $this->filter);
+        $entries = $ldap->getEntries();
+        $this->assertIsArray($entries);
+        $this->assertArrayHasKey('count', $entries);
+    }
+
+
+    public function testGetEntitiesFail()
+    {
+        $ldap  = new LdapClient();
+        $ldap->connection($this->server, $this->port, $this->user, $this->password, $this->options);
+        $ldap->search($this->baseDn.'1', $this->filter);
+        $entries = $ldap->getEntries();
+        $this->assertIsArray($entries);
+        $this->assertArrayNotHasKey('count', $entries);
+    }
+
+
+    public function testDisconnectSuccess()
+    {
+        $ldap  = new LdapClient();
+        $ldap->connection($this->server, $this->port, $this->user, $this->password, $this->options);
+        $ldap->search($this->baseDn, $this->filter);
+        $disconnection = $ldap->disconnection();
+        $this->assertTrue($disconnection);
+    }
+
+    public function testDisconnectFail()
+    {
+        $ldap  = new LdapClient();
+        $disconnection = $ldap->disconnection();
+        $this->assertFalse($disconnection);
+    }
+
+}
